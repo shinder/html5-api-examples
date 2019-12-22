dd539587e2e1f5beb8aaf99a51e21f32a46354c3
shinder <shinder.lin@gmail.com>
2019-12-22 11:21:42 +0800

    Server-Sent Event test 2

--- app.js
+++ app.js
@@ -24,35 +24,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 
 app.get('/try-sse', (req, res) => {
-    let id = 30;
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-    });
-    setInterval(function () {
-        let now = new Date();
-        res.write('id: ' + id++ + '\n');
-        res.write('data: ' + now.toLocaleString() + '\n\n');
-    }, 2000);
+	let id = 30;
+	res.writeHead(200, {
+		'Content-Type': 'text/event-stream',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive',
+	});
+	setInterval(function () {
+		let now = new Date();
+		res.write('id: ' + id++ + '\n');
+		res.write('data: ' + now.toLocaleString() + '\n\n');
+	}, 2000);
 });
 
 app.use('/', serveIndex('public', {'icons': true}));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
