d65d7397342bfc03fc112061aaa694b05c577171
shinder <shinder.lin@gmail.com>
2019-12-29 11:42:18 +0800

    單一檔案上傳 修改

--- routes/ajax-upload.js
+++ routes/ajax-upload.js
@@ -2,33 +2,48 @@ const express = require('express');
 const multer = require('multer');
 const uuidv4 = require('uuid/v4');
 const fs = require('fs');
-const upload = multer({dest: __dirname + '/../tmp-uploads'});
+const extMap = {  // 檔案類型的副檔名的對應
+	'image/jpeg': '.jpg',
+	'image/png': '.png',
+	'image/gif': '.gif',
+};
+// multer 儲存的設定
+const storage = multer.diskStorage({
+	destination: (req, file, cb)=>{
+		cb(null, __dirname + '/../public/img-uploads'); // 存放位置
+	},
+	filename: (req, file, cb)=>{
+		let ext = extMap[file.mimetype]; // 副檔名
+		if(ext)
+			cb(null, uuidv4() + ext);
+		else
+			cb(new Error('上傳的檔案格式錯誤'));
+	}
+});
+const fileFilter = (req, file, cb)=>{
+	if(extMap[file.mimetype])
+		cb(null, true); // 接受檔案
+	else
+		cb(null, false); // 不接受
+};
+const upload = multer({
+	storage: storage,
+	fileFilter: fileFilter,
+});
 const router = express.Router();
 router.post('/profile', upload.single('avatar'), function(req, res) {
 	let data; // 要存檔的資料
-	const extMap = {  // 檔案類型的副檔名的對應
-		'image/jpeg': '.jpg',
-		'image/png': '.png',
-	};
-	const output = { body: req.body }; // 先預放傳入的 POST 資料
+	const output = { body: req.body }; // 輸出給前端，先預放傳入的 POST 資料
 	try {
 		data = require(__dirname + '/../public/json/profile'); // 取資料
 	} catch (ex) {
-		console.log(ex);
-		data = {}; // 預設值
+		data = {}; // 如果找不到檔案，使用預設值
 	}
 	data.user = req.body.user; // 變更資料
 	data.description = req.body.description;
 	if(req.file && req.file.originalname){ // 若有上傳檔案
-		let ext = extMap[req.file.mimetype]; // 副檔名
-		if(ext) {
-			output.upload = uuidv4() + ext; // 隨機檔名接上副檔名
-			data.avatar = '/img-uploads/' + output.upload; // 儲存包含路徑
-			fs.rename(req.file.path, __dirname + '/../public' + data.avatar, error=>{});
-		} else {
-			output.error = '上傳的檔案格式錯誤';
-			fs.unlink(req.file.path, error=>{}); // 刪除暫存檔
-		}
+		data.avatar = '/img-uploads/' + req.file.filename; // 儲存包含路徑
+		output.upload = data.avatar;
 	}
 	fs.writeFile(__dirname+'/../public/json/profile.json', JSON.stringify(data), error=>{
 		if(error) return console.log(error);
